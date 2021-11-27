9fb9f799a589d21e2a7bf97e61afbcfed85471ce
Asj-code <andreasilvinaj@gmail.com>
2021-11-27 20:56:50 -0300

    Project start.

---
+++ MoviesOnline/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoviesOnline
+//
+//  Created by Avalith on 11/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("Hello Mauricio !!")
+    }
+
+
+}
+
