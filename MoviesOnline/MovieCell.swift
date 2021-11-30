e8ee58fb8c506a0a278214e1036727388159507d
Asj-code <andreasilvinaj@gmail.com>
2021-11-29 23:17:51 -0300

    vertical List initialization.

---
+++ MoviesOnline/MovieCell.swift
@@ -0,0 +1,19 @@
+//
+//  MovieCell.swift
+//  MoviesOnline
+//
+//  Created by Avalith on 11/29/21.
+//
+
+import Foundation
+import UIKit
+
+class MovieCell: UITableViewCell{
+    
+    
+    @IBOutlet weak var titleTv: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
--- MoviesOnline/ViewController.swift
+++ MoviesOnline/ViewController.swift
@@ -8,13 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    var moviesLst = [""]
+    let reuseIdentifier = "movieCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        moviesLst = ["Noticia 1", "Noticia 2", "Noticia 3", "Noticia 4", "Noticia 5"]
         print("Hello Mauricio !!")
     }
 
 
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return moviesLst.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)as! MovieCell
+        cell.titleTv.text = moviesLst[indexPath.row]
+        return cell
+    }
 }
 
